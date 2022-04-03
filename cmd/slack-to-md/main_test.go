67224194709f2eb66313ea3f1a9d51cdfb220394
Robin Neatherway <rneatherway@github.com>
2022-04-03 19:11:28 +0100

    Fix test

--- cmd/slack-to-md/main_test.go
+++ cmd/slack-to-md/main_test.go
@@ -4,8 +4,10 @@ import "testing"
 
 func TestPermalinkRE(t *testing.T) {
 	result := permalinkRE.FindStringSubmatch("https://github.slack.com/archives/CP9GMKJCE/p1648028606962719")
-	for _, r := range result {
-		t.Log(r)
+	if len(result) != 4 {
+		t.Errorf("result had length %d", len(result))
 	}
-	t.Fail()
+	if result[1] != "CP9GMKJCE" || result[2] != "1648028606" || result[3] != "962719" {
+		t.Fail()
+	}
 }
