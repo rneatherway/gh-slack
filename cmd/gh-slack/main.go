057d58271242362a93b089399a0324ecc3e07789
Robin Neatherway <rneatherway@github.com>
2022-05-10 18:09:34 +0100

    WIP linux getter

--- cmd/gh-slack/main.go
+++ cmd/gh-slack/main.go
@@ -9,12 +9,12 @@ import (
 	"os"
 	"regexp"
 
+	"github.com/jessevdk/go-flags"
 	"github.com/rneatherway/gh-slack/internal/gh"
 	"github.com/rneatherway/gh-slack/internal/markdown"
 	"github.com/rneatherway/gh-slack/internal/slackclient"
 	"github.com/rneatherway/gh-slack/internal/version"
-
-	"github.com/jessevdk/go-flags"
+	"r00t2.io/gosecret"
 )
 
 var (
@@ -45,7 +45,110 @@ var opts struct {
 	Issue   string `short:"i" long:"issue" description:"The URL of a repository to post the output as a new issue, or the URL of an issue to add a comment to that issue"`
 }
 
+const (
+	// The default collection; it should be available on all SecretService implementations.
+	collectionName string = "login"
+	// A label for an Item used in examples below.
+	exampleLabel string = "Some Website Credentials"
+)
+
+func test() error {
+
+	var err error
+	var service *gosecret.Service
+	//var collection *gosecret.Collection
+	// var item *gosecret.Item
+	// var itemAttrs map[string]string
+	var itemLabel string
+	//var secret *gosecret.Secret
+
+	// All interactions with SecretService start with initiating a Service connection.
+	if service, err = gosecret.NewService(); err != nil {
+		log.Panicln(err)
+	}
+	defer service.Close()
+
+	// And unless operating directly on a Service via its methods, you probably need a Collection as well.
+	// if collection, err = service.GetCollection(collectionName); err != nil {
+	// 	log.Panicln(err)
+	// }
+
+	/*
+		Create a Secret which gets stored in an Item which gets stored in a Collection.
+		See the documentation for details.
+	*/
+	// Incidentally, I believe this is the only exported function/method that does not return an error returner.
+	// secret = gosecret.NewSecret(
+	// 	service.Session,                   // The session associated with this Secret. You're likely fine with the automatically-created *(Service).Session.
+	// 	[]byte{},                          // The "parameters". Likely this is an empty byteslice.
+	// 	[]byte("a super secret password"), // The actual secret value.
+	// 	"text/plain",                      // The content type (MIME type/media type). See https://www.iana.org/assignments/media-types/media-types.xhtml.
+	// )
+
+	/*
+		Item attributes are a map[string]string of *metadata* about a Secret/Item.
+		Do *NOT* store sensitive information in these.
+		They're primarily used for searching for Items.
+	*/
+	itemAttrs := map[string]string{
+		"xdg:schema":  "chrome_libsecret_os_crypt_password_v2",
+		"application": "Slack",
+	}
+
+	// // And create the Item (and add it to SecretService).
+	// if item, err = collection.CreateItem(
+	// 	exampleLabel, // The label of the item. This should also be considered not secret.
+	// 	itemAttrs,    // Attributes for the item; see above.
+	// 	secret,       // The actual secret.
+	// 	true,         // Whether to replace an existing item with the same label or not.
+	// ); err != nil {
+	// 	log.Panicln(err)
+	// }
+
+	/*
+		Now let's fetch the same Item via its attributes.
+		The results are split into locked items and unlocked items.
+	*/
+	var unlockedItems []*gosecret.Item
+	//var lockedItems []*gosecret.Item
+
+	if unlockedItems, _, err = service.SearchItems(itemAttrs); err != nil {
+		log.Panicln(err)
+	}
+
+	// We should only have one Item that matches the search attributes, and unless the item or collection is locked, ...
+	item := unlockedItems[0]
+	if itemLabel, err = item.Label(); err != nil {
+		log.Panicln(err)
+	}
+	fmt.Printf("Found item: %v %v\n", itemLabel, string(item.Secret.Value))
+
+	// Alternatively if you are unsure of the attributes but know the label of the item you want, you can iterate through them.
+	// var itemResults []*gosecret.Item
+
+	// if itemResults, err = collection.Items(); err != nil {
+	// 	log.Panicln(err)
+	// }
+	// fmt.Printf("Found %d items\n", len(itemResults))
+	// for idx, i := range itemResults {
+	// 	if itemLabel, err = i.Label(); err != nil {
+	// 		fmt.Printf("Cannot read label for item at path '%v'\n", i.Dbus.Path())
+	// 		continue
+	// 	}
+	// 	// if itemLabel != exampleLabel { // Matching against a desired label - exampleLabel, in this case.
+	// 	// 	continue
+	// 	// }
+	// 	fmt.Printf("Found item labeled '%v'! Index number %v at path '%v'\n", itemLabel, idx, i.Dbus.Path())
+	// 	fmt.Println(i.Attrs)
+	// 	//fmt.Printf("Password: %v\n", string(i.Secret.Value))
+	// 	//break
+	// }
+	return nil
+}
+
 func realMain() error {
+	test()
+	return nil
 	_, err := flags.NewParser(&opts, flags.HelpFlag|flags.PassDoubleDash).Parse()
 	if err != nil {
 		return err
--- go.mod
+++ go.mod
@@ -6,11 +6,13 @@ require (
 	github.com/cli/go-gh v0.0.3
 	github.com/jessevdk/go-flags v1.5.0
 	modernc.org/sqlite v1.15.3
+	r00t2.io/gosecret v1.1.5
 )
 
 require (
 	github.com/cli/safeexec v1.0.0 // indirect
 	github.com/cli/shurcooL-graphql v0.0.1 // indirect
+	github.com/godbus/dbus/v5 v5.0.6 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/henvic/httpretty v0.0.6 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
@@ -18,7 +20,7 @@ require (
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
 	golang.org/x/mod v0.3.0 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
 	golang.org/x/tools v0.0.0-20201124115921-2c860bdd6e78 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
@@ -31,4 +33,5 @@ require (
 	modernc.org/opt v0.1.1 // indirect
 	modernc.org/strutil v1.1.1 // indirect
 	modernc.org/token v1.0.0 // indirect
+	r00t2.io/goutils v1.1.2 // indirect
 )
